b6cefbe59e7437d7e2de61c7dbfda8720d3546af
אילה אפטרגוט <tmunot1234567@gmail.com>
2023-09-14 14:39:55 +0300

    adding a tamplate

---
+++ wwwroot/js/script.js
@@ -0,0 +1,44 @@
+let navbar = document.querySelector('.header .navbar');
+let menuBtn = document.querySelector('#menu-btn');
+
+menuBtn.onclick = () =>{
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menuBtn.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+
+var swiper = new Swiper(".home-slider", {
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+});
+
+var swiper = new Swiper(".reviews-slider", {
+    pagination:{
+        el:".swiper-pagination",
+        clickable:true,
+    },
+    grabCursor:true,
+    loop:true,
+    spaceBetween:20,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
+});
