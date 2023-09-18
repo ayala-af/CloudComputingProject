a4ad6a988879f45f1434292d275da55cdf7956f9
Efrat Weksler <eweksler@g.jct.ac.il>
2023-09-18 14:59:30 +0300

    Views added through homecontroller, css pallette testing

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,10 +18,19 @@ namespace CloudComputingProject.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
+        public IActionResult Menu()
+        {
+            return View();
+        }
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
