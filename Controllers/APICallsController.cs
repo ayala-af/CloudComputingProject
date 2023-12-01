8005485fd08f28d39d844b330a03e8a8fa2c73ce
ef-w <efratart2161@gmail.com>
2023-12-01 03:32:30 +0200

    calls of the cloud based gateway - bool api calls

---
+++ Controllers/APICallsController.cs
@@ -0,0 +1,28 @@
+﻿using CloudComputingProject.Models;
+using System.Net;
+using System.Text.Json;
+
+public static class APICallsController
+{
+	public static async Task<bool> IsHebrewHolidayAsync()
+	{
+		using (var client = new HttpClient())
+		{
+			var apiEndpoint = "http://www.apigateway.somee.com/HebrewDate/IsHebrewHoliday";
+			var response = await client.GetStringAsync(apiEndpoint);
+			return bool.Parse(response);
+		}
+	}
+	
+	public static async Task<bool> IsActualLocationAsync(string city, string street)
+	{
+		var apiEndpoint = $"http://www.apigateway.somee.com/Address?city={city}&{street}";
+
+		using (var client = new HttpClient())
+		{
+			var response = await client.GetStringAsync(apiEndpoint);
+			return bool.Parse(response);
+		}
+	}
+}
+
